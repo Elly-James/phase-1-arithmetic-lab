4ecd808dcef20af80cde84d5c1a87c162e78261b
Elly <ellyjames1999@gmail.com>
2024-11-28 12:18:18 +0300

    finished commiting

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let multiply;
+let num1=31, num2=2;
+multiply = num1*num2;
+console.log(multiply);
+
+let random =Math.floor(Math.random()*10 + 1);
+console.log(random);
+
+let mod;
+let num3=11, num4=7;
+mod= num3% num4;
+console.log(mod);
+
+let max= Math.max(2,3,4,5,13,15,6,19,13,20);
+console.log(max);
